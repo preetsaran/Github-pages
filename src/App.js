65aff3fe3cd9c1dc47bfd80bda87c824c39ffca1
Saranpreet Singh <sarans669@gmail.com>
2022-09-23 23:31:23 +0530

    Style improvements

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import IssuePage from "./components/IssuePage";
 import Header from "./components/Header";
-import { Paper, Box } from '@mui/material';
+import { Box } from '@mui/material';
 
 function App() {
   return (
     <Box bgcolor='#0d1117'>
       <Header />
-      <Box >
-        <IssuePage />
-      </Box>
+      <IssuePage />
     </Box>
   );
 }
